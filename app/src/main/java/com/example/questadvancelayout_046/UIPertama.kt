3f07568e9462d9906d3be6abfb8e4020eab067b9
anandajuliar <anandajulia001@gmail.com>
2025-10-24 15:57:15 +0700

    membuat padding bawah pd text

--- app/src/main/java/com/example/questadvancelayout_046/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_046/UIPertama.kt
@@ -90,6 +90,7 @@ fun ActivitasPertama(modifier: Modifier) {
                 text = stringResource(id = R.string.copy),
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
             )
         }
         }
