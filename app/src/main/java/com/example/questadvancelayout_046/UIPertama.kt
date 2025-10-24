2f4452b42d02a454a8f4d0807b97425d2d084c49
anandajuliar <anandajulia001@gmail.com>
2025-10-24 16:06:55 +0700

    fixing err

--- app/src/main/java/com/example/questadvancelayout_046/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_046/UIPertama.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -65,12 +66,12 @@ fun ActivitasPertama(modifier: Modifier) {
                         .padding(all = 5.dp)
                 )
                 Spacer(modifier = Modifier.width(width = 30.dp))
-                Column() {
+                Column(){
                     Text(
                         text = stringResource(id = R.string.nama),
                         fontSize = 30.sp,
                         fontFamily = FontFamily.Cursive,
-                        Color= Color.White,
+                        color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
                     Text(
