1249159d988b52692149d30b9436fe4faf45bc07
anandajuliar <anandajulia001@gmail.com>
2025-10-24 15:30:28 +0700

    membuat row dalam card

--- app/src/main/java/com/example/questadvancelayout_046/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_046/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.questadvancelayout_046
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -44,7 +46,12 @@ fun ActivitasPertama(modifier: Modifier) {
                 containerColor = Color.DarkGray
             )
         ){
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(all=10.dp)
+            ){
 
+            }
         }
         }
     }
