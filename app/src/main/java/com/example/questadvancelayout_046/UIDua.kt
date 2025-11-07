c81fc2d6be459c2bcd000a3a29cba69ef66c7078
anandajuliar <anandajulia001@gmail.com>
2025-11-07 10:36:28 +0700

    membuat fun social media icon

--- app/src/main/java/com/example/questadvancelayout_046/UIDua.kt
+++ app/src/main/java/com/example/questadvancelayout_046/UIDua.kt
@@ -190,4 +190,24 @@ fun PraktikumPage(modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun SocialMediaIcon(
+    icon: ImageVector
+) {
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .background(Color(0xFF4169E1))
+            .border(2.dp, Color(0xFF4169E1), CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = "Social Media",
+            tint = Color.White,
+            modifier = Modifier.size(20.dp)
+        )
+    }
 }
